eb45bf63fab1c2cf236db929a6c82c8d06892c82
Jerome Badt <jerome.badt.extern@s-management-services.de>
2024-08-22 12:22:23 +0200

    initial commit

---
+++ src/main/java/btb/gmbh/recipebot/RecipebotApplication.java
@@ -0,0 +1,13 @@
+package btb.gmbh.recipebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipebotApplication.class, args);
+	}
+
+}
